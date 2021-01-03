1f24bf7045cf3ea6d3c4cd7c276e74d56f41b825
GokulMurugan16 <Gokul16.619@gmail.com>
2021-01-03 18:35:08 -0500

    Added Text fields

--- Saga Plus/Saga Plus/ViewController.swift
+++ Saga Plus/Saga Plus/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var E_Mail: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: Login Button
+    @IBAction func Login_Button(_ sender: Any) {
+        
+        print(E_Mail.text)
+        print(Password)
+        
+        
+        
+    }
+    
 
 }
 
